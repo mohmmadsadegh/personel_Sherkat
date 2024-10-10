5ab38331080736731361e18ca87b4c77b054afd5
mohmmadsadegh <mohmmadsadeghnajafi06@gmail.com>
2024-10-10 15:18:05 +0330

    sabeghkifary,bedehi,saatkar تکمیل محافظت از متغییرهای

--- personel.py
+++ personel.py
@@ -6,6 +6,7 @@ class person(ABC):
     age=0
     jens=bool
     tel=''
+    sabeghKifary=""
     @abstractmethod
     def __init__(self,name,famili,age,jens,tel) -> None:
         self.name=name
@@ -13,7 +14,6 @@ class person(ABC):
         self.age=age
         self.jens=jens
         self.tel=tel
-        
 class company():
     name=""
     place=""
@@ -40,30 +40,54 @@ class company():
         self.Moshtariyan.append(moshtari)    
     def Show_Moshtari(self):
         for item in self.Moshtariyan:        
-            print(f"list moshtariyan: {item.name} {item.famili} kalaye sefarshi shoma {item.kala} hast va tedad kala {item.number_kala} adad mibashad  ba ghimat {item.price}$")         
+            print(f"list moshtariyan: **{item.name}**  **{item.famili}**  kalaye sefarshi shoma **{item.kala}**  hast va tedad kala **{item.number_kala}**  adad mibashad  ba ghimat **{item.price}$** **{item.bedehi}**")         
     def Add_taminkonande(self,taminkonande):
         self.Taminkonandegan.append(taminkonande) 
     def Show_taminkonande(self):
         for item in self.Taminkonandegan:
-            print(f"list taminkonandegan {item.name} tahiye shode az {item.mavad_Avaliye} ba ghimate {item.price_mavad_avaliye}$ ba tahvil {item.tahvil}")     
+            print(f"list taminkonandegan **{item.name}**  tahiye shode az **{item.mavad_Avaliye}**  ba ghimate **{item.price_mavad_avaliye}$**  ba tahvil **{item.tahvil}** {item.sabeghKifary}")     
     def Add_personel(self,personel):
         self.personels.append(personel) 
     def Show_personel(self):
         for item in self.personels:
-            print(f"list karkonan {item.name} {item.famili} {item.takhasos}")                             
+            print(f"list karkonan **{item.name}**  **{item.famili}**   **{item.takhasos}**  **{item.saatKar}** **{item.sabeghKifary}**")                             
 class personel(person):
     takhasos=''
     saatKar=0
     dastmozd=''
     history_job=0 
     hesabdar='' 
-    boss_All=''  
-    def __init__(self, name, famili,takhasos,saatKar,dastmozd) -> None:
+    boss_All='' 
+    sabeghKifary=False 
+    def __init__(self, name, famili,takhasos,saatKar,dastmozd,sabeghKifary:str|int|bool) -> None:
         self.name=name
         self.famili=famili
         self.takhasos=takhasos
-        self.saatKar=saatKar
+        self._saatKar=saatKar
         self.dastmozd=dastmozd
+        self._sabeghKifary=sabeghKifary       
+    @property
+    def saatKar(self):
+        print("GET")    
+        return f"{self._saatKar} saat kar mikonad dar rooz"
+    @saatKar.setter
+    def saatKar(self,value):
+        print("SET")
+        if value<=0:
+            self._saatKar=0
+        else:
+            self._saatKar=value 
+    @property
+    def sabeghKifary(self):
+        print("Get3")
+        return f' sabegh keyfari shoma {self._sabeghKifary} is '      
+    @sabeghKifary.setter
+    def sabeghKifary(self,value):
+        print("SET3")
+        if value !=False:
+            self._sabeghKifary=value
+        else:
+            self._sabeghKifary=0               
 class moshtari(person):
     History_shopping=""
     addres=""
@@ -72,18 +96,30 @@ class moshtari(person):
     number_kala=0
     companys=[]
     hesabdari=''
-    def __init__(self,name,famili,kala,price,number_kala) -> None:
+    bedehi=0
+    def __init__(self,name,famili,kala,price,number_kala,bedehi) -> None:
         self.name=name
         self.famili=famili
         self.kala=kala
         self.price=price
         self.number_kala=number_kala
+        self._bedehi=bedehi
         self.companys=[]
     def Add_Company(self,company):
         self.companys.append(company)    
     def Show_company(self):
         for item in self.companys:
-            print(f" company:{item.name} {item.place} {item.kala} {item.price}$")    
+            print(f" company:**{item.name}**  **{item.place}**  **{item.kala}**  **{item.price}$**")    
+    @property 
+    def bedehi(self):
+        print("GET2")   
+        return f"shoma {self._bedehi} bedehi az ghabl darid"    
+    @bedehi.setter
+    def bedehi(self,value):
+        if value<=0:
+            self._bedehi=0
+        else:
+            self._bedehi=value     
 class Tamin(person):
     mavad_Avaliye=''
     price_mavad_avaliye=0
@@ -101,7 +137,7 @@ class Tamin(person):
         self.companys.append(company)    
     def Show_company(self):
         for item in self.companys:
-            print(f"{item.name} {item.place} {item.kala} {item.price}")    
+            print(f"**{item.name}**  **{item.place}**  **{item.kala}**  **{item.price}**")    
     
 class Hesabdari(person):
     tasviye_Moshtari= bool   
@@ -121,17 +157,17 @@ class Hesabdari(person):
         self.Moshtariyan.append(moshtari)    
     def Show_Moshtari(self):
         for item in self.Moshtariyan:        
-            print(f" list moshtariyan: {item.name} {item.famili} kalay sefareshi shoma {item.kala}  mibashad va tedad kala  {item.number_kala} addad mibashad ba ghimat {item.price}$")    
+            print(f" list moshtariyan: **{item.name}**  **{item.famili}**  kalay sefareshi shoma **{item.kala}**  mibashad va tedad kala  **{item.number_kala}** addad mibashad ba ghimat **{item.price}$** **{item.bedehi}**")    
     def Add_taminkonande(self,taminkonande):
         self.Taminkonandegan.append(taminkonande) 
     def Show_taminkonande(self):
         for item in self.Taminkonandegan:
-            print(f" list taminkonandegan: {item.name} tahiye shode az  {item.mavad_Avaliye} ba ghimat {item.price_mavad_avaliye}$ dar  {item.tahvil}")                       
+            print(f" list taminkonandegan: **{item.name}** tahiye shode az  **{item.mavad_Avaliye}** ba ghimat **{item.price_mavad_avaliye}$**  dar  **{item.tahvil}**")                       
     def Add_personel(self,personel):
         self.personels.append(personel) 
     def Show_personel(self):
         for item in self.personels:
-            print(f" list personel:  {item.name}  {item.famili} {item.takhasos}")                             
+            print(f" list personel:  **{item.name}**  **{item.famili}**  **{item.takhasos}**  **{item.saatKar}**  **{item.sabeghKifary}**")                             
     
 class Boss_company(person):
     takhasos=""
@@ -154,17 +190,17 @@ class Boss_company(person):
         self.Moshtariyan.append(moshtari)
     def Show_Moshtari(self):
         for item in self.Moshtariyan:        
-            print(f" list moshtariyan: {item.name} {item.famili} kalay sefareshi shoma {item.kala} mibashad ba tedad sefaresh {item.number_kala} addad ba ghimat {item.price}$")    
+            print(f" list moshtariyan: **{item.name} **  **{item.famili}**  kalay sefareshi shoma  **{item.kala}**  mibashad ba tedad sefaresh  **{item.number_kala}**  addad ba ghimat **{item.price}$** **{item.bedehi}**")    
     def Add_taminkonande(self,taminkonande):
         self.Taminkonandegan.append(taminkonande) 
     def Show_taminkonande(self):
         for item in self.Taminkonandegan:
-            print(f" list taminkonandegan: {item.name} tahiye shode az  {item.mavad_Avaliye} ba ghimat  {item.price_mavad_avaliye}$ dar {item.tahvil}")         
+            print(f" list taminkonandegan: **{item.name}**  tahiye shode az  **{item.mavad_Avaliye}** ba ghimat  **{item.price_mavad_avaliye}$**  dar  **{item.tahvil}**")         
     def Add_personel(self,personel):
         self.personels.append(personel) 
     def Show_personel(self):
         for item in self.personels:
-            print(f" list personel: {item.name} {item.famili} {item.takhasos}")                             
+            print(f" list personel: **{item.name}**  **{item.famili}**  **{item.takhasos}**  **{item.saatKar}** **{item.sabeghKifary}**")                             
 # companyنمونه گیری از کلاس         
 company1=company("samsung","south korea","mobile & pc ",8500,123456)    
 company2=company("xiaomi","china","mobile & pc ",7500,123456)    
@@ -173,17 +209,32 @@ company4=company("meta","usa","social network",145000,123456)
 company5=company("souni","south korea & china","mobile & Game console  ",9500,123456)    
 company6=company("pixel","usa","mobile",9000,123456)    
 # personelنمونه گیری از کلاس 
-personel1=personel("mark","zakerberg","Boss",8,5000000000)
-personel2=personel("emma","zilinski","secretary",10,4200)
-personel3=personel("edmond","mikhtariyan","driver",12,4500)
-personel4=personel("morad","onder","cook",12,3500)
-personel5=personel("yorgen","ta","Data spiecialist",10,4800)
+personel1=personel("mark","zakerberg","Boss",8,5000000000,False)
+personel1.saatKar=8
+personel1.sabeghKifary=False
+personel2=personel("emma","zilinski","secretary",10,4200,False)
+personel2.saatKar=10
+personel2.sabeghKifary=1
+personel3=personel("edmond","mikhtariyan","driver",12,4500,False)
+personel3.sabeghKifary=True
+personel3.saatKar=12
+personel4=personel("morad","onder","cook",12,3500,False)
+personel4.saatKar=6
+personel4.sabeghKifary=True
+personel5=personel("yorgen","ta","Data spiecialist",10,4800,False)
+personel5.sabeghKifary=True
+personel5.saatKar=9
 #moshtariنمونه گیری از 
-moshtari1=moshtari("sadra","saidi","mobil",1500,1)
-moshtari2=moshtari("yavar","toghrol","pc",1000,2)
-moshtari3=moshtari("bijan","mortazavi","consol",1200,1)
-moshtari4=moshtari("bayram","bagali","consol",1200,1)
-moshtari5=moshtari("amirmansor","pashazade","pc",800,1)
+moshtari1=moshtari("sadra","saidi","mobil",1500,1,0)
+moshtari1.bedehi=0
+moshtari2=moshtari("yavar","toghrol","pc",1000,2,0)
+moshtari2.bedehi=0
+moshtari3=moshtari("bijan","mortazavi","consol",1200,1,0)
+moshtari3.bedehi=1
+moshtari4=moshtari("bayram","bagali","consol",1200,1,0)
+moshtari4.bedehi=2
+moshtari5=moshtari("amirmansor","pashazade","pc",800,1,0)
+moshtari5.bedehi=4
 # taminنمونه گیری از کلاس 
 Tamin1=Tamin("app_Stor","vscode",15,"haman lahze")
 Tamin2=Tamin("google play","pycharm",15,"haman lahze")
